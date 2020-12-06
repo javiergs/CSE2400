43e30372ab68005cb1323093ea03fccc75534e2b
javiergs <mscjaviergs@gmail.com>
2020-12-05 20:46:44 -0700

    Java update
    New examples

---
+++ 01 Language Java/Test Two/Classroom.java
@@ -0,0 +1,75 @@
+/*
+ * Author: Dragan Bogoevski
+ * Description: Classroom class containing information about two dimensional classrooms that can be filled with students 
+ * and appropriate methods and constructors to work with this class
+ */
+
+public class Classroom {
+
+	private Student[][] arrangement;
+
+	/*
+	 * Parameterized constructor with dimensions of the classroom as input
+	 * Initializes the full classroom with students with default values
+	 */
+	public Classroom(int rowNum, int columnNum) {
+		arrangement = new Student[rowNum][columnNum];
+
+		for (int i = 0; i < rowNum; i++) {
+			for (int j = 0; j < columnNum; j++) {
+				arrangement[i][j] = new Student();
+			}
+		}
+	}
+
+	/*
+	 * Returns the Student object at the specific location
+	 */
+	private Student getStudentAt(int row, int col) {
+		return arrangement[row][col];
+	}
+
+	/*
+	 * Tries to place a student at a specific location in the classroom If that
+	 * location is free, i.e. is filled by a student with default values, the
+	 * parameter student is placed there and the method returns true; If the
+	 * location is already taken by another student that is not a default student,
+	 * the method returns false;
+	 */
+	public boolean setStudentAt(int row, int col, Student data) {
+		var existingStudent = getStudentAt(row, col);
+		if (existingStudent.getFirstName() == "bar" && existingStudent.getLastName() == "foo") {
+			arrangement[row][col] = data;
+			return true;
+		} else
+			return false;
+	}
+
+	/*
+	 * Checks whether given values for row and column are valid (in bounds) for the
+	 * classroom
+	 */
+	public boolean isValid(int row, int col) {
+		if (col < 0 || col >= arrangement.length || row < 0 || row >= arrangement[0].length)
+			return false;
+		return true;
+	}
+
+	/*
+	 * Returns a string giving full visualization of the classroom containing the
+	 * student's initials or the full name if the student is a default student
+	 */
+	public String toString() {
+		String returnString = "The current seating:";
+		returnString += System.lineSeparator();
+
+		for (int i = 0; i < arrangement[0].length; i++) {
+			String rowData = "";
+			for (int j = 0; j < arrangement.length; j++) {
+				rowData += arrangement[i][j].toString() + " ";
+			}
+			returnString += rowData + System.lineSeparator();
+		}
+		return returnString;
+	}
+}
---
+++ 01 Language Java/Test Two/Student.java
@@ -0,0 +1,53 @@
+/*
+ * Author: Dragan Bogoevski
+ * Description: Student class containing information about first name, last name 
+ * and appropriate methods and constructors to work with this class
+ */
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+
+	/*
+	 * Parameterless constructor with default values
+	 */
+	public Student() {
+		lastName = "foo";
+		firstName = "bar";
+	}
+
+	/*
+	 * Parameterized constructor with full name as a parameter containing / between
+	 * first and last name e.g. John/Doe
+	 */
+	public Student(String info) {
+		var splitInfo = info.split("/");
+		firstName = splitInfo[0];
+		lastName = splitInfo[1];
+	}
+
+	/*
+	 * Returns the student's last name
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/*
+	 * Returns the student's first name
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/*
+	 * Returns the student's initials e.g. for John/Doe will return J.D. For default
+	 * values returns full first and last name
+	 */
+	public String toString() {
+		if (firstName == "bar" && lastName == "foo")
+			return firstName + "." + lastName + ".";
+		return firstName.charAt(0) + "." + lastName.charAt(0) + ".";
+	}
+}
